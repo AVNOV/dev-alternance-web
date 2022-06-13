461ddfa86297c0627dbc41282ead638f668accdf
Arthur VIARD <arthur.viard@ynov.com>
2022-06-13 17:08:01 +0200

    [ADD] Set input data of ActiviteJour in local storage for centralized data treatment for the futur mysql register creation

--- creat/src/App.tsx
+++ creat/src/App.tsx
@@ -1,53 +1,20 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
 import { Provider } from "react-redux";
 import { store } from "./store/exploitReduxStore";
-import Login from './components/Login/Login';
-import Logout from './components/Login/Logout';
-import Profile from './components/Login/Profile';
 import { useAuth0 } from '@auth0/auth0-react'
 import LoadingSpinner from './components/Spinner/LoadingSpinner';
-import ActiviteJour from './components/SaisieActivite/ActiviteJour';
-import Header from './components/Template/Header';
-import SelectImmatriculation from './components/SaisieActivite/SelectImmatriculation';
-import NewActivity from './components/SaisieActivite/NewActivity';
-import Footer from './components/Template/Footer';
-import SetKilometrage from './components/SaisieActivite/SetKilometrage';
-import FooterNav from './components/Template/Footer';
-import SelectTournee from './components/SaisieActivite/SelectTournee';
-import SetActivityPoints from './components/SaisieActivite/SetActivityPoints';
+import AppRouter from './components/AppRouter';
+import darkTheme from './Style/MaterialTheme';
 
 function App() {
-  
-  	const { isLoading } = useAuth0(); 
-
-    const darkTheme = createTheme({
-        palette: {
-            mode: 'dark',
-        },
-    });
-
-    if (isLoading) return (<div className='loading'><LoadingSpinner /></div>);
+//  	const { isLoading } = useAuth0(); 
+//    if (isLoading) return (<div className='loading'><LoadingSpinner /></div>);
 
     return (
         <>
           <ThemeProvider theme={darkTheme}>
                 <Provider store={store}>
-                    <Router>
-                        <Routes>
-	    		            <Route path="/" element={<><Header/><NewActivity/></>} />
-                            <Route path='/login' element={<><div><Login /></div><Logout /><Profile /></>} /> 
-                        </Routes>
-                        <Routes>
-                            <Route path='/dailyActivity' element={<><Header /><ActiviteJour /><FooterNav /></>} />
-                            <Route path='/vehiculeActivity' element={<><Header /><SelectImmatriculation /><SetKilometrage /><Footer /></>} />
-                            <Route path='/selectTournee' element={<><Header /><SelectTournee /><FooterNav /></>} />
-                            <Route path='/setActivityPoints' element={<><Header /><SetActivityPoints /><FooterNav /></>} />
-                            {/* Choix 1: Faire une modal de recapitulatif de saisie d'activité */}
-                            {/* choix 2: Faire une route spéciale pour le récapitulatif "/recap" */}
-                            <Route path='/recap' element={<><Header /><p>recap</p><FooterNav /></>} />
-                        </Routes>
-                    </Router>
+                    <AppRouter />
                 </Provider>
             </ThemeProvider>
         </>
--- creat/src/components/SaisieActivite/ActiviteJour.tsx
+++ creat/src/components/SaisieActivite/ActiviteJour.tsx
@@ -1,40 +1,59 @@
 import { Box, Grid, TextField, Typography } from "@mui/material";
-import { Fragment, useState } from "react";
-import FooterNav from "../Template/Footer";
+import { SetStateAction, SyntheticEvent } from "react";
 import InformationModal from "../Template/InformationModal";
 import './Activite.css';
 
-function ActiviteJour() : JSX.Element{
-    const [hourStart, setHourStart] = useState<string>('');
-    const [hourEnd, setHourEnd] = useState<string>('');
-    const [breakTime, setBreakTime] = useState<string>('');
+type ActiviteJourProps = {
+    hourStart: string,
+    setHourStart: (hourStart: string) => void,
+    hourEnd: string,
+    setHourEnd: (hourEnd: string) => void,
+    breakTime: string
+    setBreakTime: (breakTime: string) => void,
+};
 
-/*    
-    const handlePrint = () => {
-        console.log(hourStart);
-    };
-*/    
+// const setHourStart = ({ hourStart, setHourStart }: ActiviteJourProps): JSX.Element => {
+//     return (
+//         <div>
+//             <input
+//                 type="text"
+//                 value={hourStart}
+//                 onChange={(e) => setHourStart(e.currentTarget.value)}
+//             />
+//         </div>
+//     );
+// };
+
+const ActiviteJour = (props: any) => {
     const info = { id: 1, msg: "Ici saisissez vos horaires." };
 
+    const handleStartInput = (evt: any) => {
+        props.onStartInput(evt.target.value);
+    };
+    const handleEndInput = (evt: any) => {
+        props.onEndInput(evt.target.value);
+    };
+    const handleBreakInput = (evt: any) => {
+        props.onBreakInput(evt.target.value);
+    };
+
     return (
-        <Fragment>
+        <div>
             <InformationModal {...info}/>
             <Grid container justifyContent={"center"}>
                 <Box sx={{ display: 'grid', mt: 5 }}>
-
-                        <Typography variant="overline" display="block" >Heure d'embauche :</Typography>
-                        <TextField sx={{ boxShadow: 1, input: { color: 'blue' } }} value={hourStart} onChange={(e) => setHourStart(e.target.value)} type="time"/>
-                        <br />
-                        <Typography variant="overline" display="block" >Heure de débauche :</Typography>
-                        <TextField sx={{ boxShadow: 1, input: { color: 'blue' } }} value={hourEnd} onChange={(e) => setHourEnd(e.target.value)} type="time"/>
-                        <br />
-                        <Typography color={"red"} variant="overline" display="block" >Temps de pause :</Typography>
-                        <TextField  sx={{ boxShadow: 1, input: { color: 'orange' } }} value={breakTime} onChange={(e) => setBreakTime(e.target.value)} type="number"/>
+                    <Typography variant="overline" display="block" >Heure d'embauche :</Typography>
+                    <TextField sx={{ boxShadow: 1, input: { color: 'blue' } }} onChange={handleStartInput} type="time"/>
+                    <br />
+                    <Typography variant="overline" display="block" >Heure de débauche :</Typography>
+                    <TextField sx={{ boxShadow: 1, input: { color: 'blue' } }} onChange={handleEndInput} type="time"/>
+                    <br />
+                    <Typography color={"red"} variant="overline" display="block" >Temps de pause :</Typography>
+                    <TextField  sx={{ boxShadow: 1, input: { color: 'orange' } }} onChange={handleBreakInput} type="number"/>
                 </Box>
             </Grid>
-        </Fragment>
+        </div>
     );
 }
 
 export default ActiviteJour;
-            
